2768cb9714948664f9c86a4530fba42e35bcf447
Axel Pettersson <axel.pettersson@northvolt.com>
2024-12-02 06:01:32 +0100

    Finish day 1

--- aoc24/day1/solution.py
+++ aoc24/day1/solution.py
@@ -1,14 +1,32 @@
-from typing import List
+from collections import Counter
+from typing import List, Tuple
+
+Input = List[Tuple[int, int]]
 
 
-def part1(lines: List[str]) -> None:
-    pass
+def parse_input(lines: List[str]) -> Input:
+    return [(int(line.split()[0]), int(line.split()[1])) for line in lines]
 
 
-def part2(lines: List[str]) -> None:
-    pass
+def part1(lines: Input) -> None:
+    left = sorted(x[0] for x in lines)
+    right = sorted(x[1] for x in lines)
+    pairs = list(zip(left, right))
+    distances = sum(abs(a - b) for a, b in pairs)
+    print("Part 1:", distances)
+
+
+def part2(lines: Input) -> None:
+    counter = Counter(right for _, right in lines)
+
+    similarity_score = 0
+    for left, _ in lines:
+        similarity_score += left * counter.get(left, 0)
+
+    print("Part 2:", similarity_score)
 
 
 def main(lines: List[str]) -> None:
-    part1(lines)
-    part2(lines)
+    input = parse_input(lines)
+    part1(input)
+    part2(input)
