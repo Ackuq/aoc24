acb935997a717b611dfb68669c9b820bea3b7bff
Axel Pettersson <axel.pettersson@northvolt.com>
2024-12-03 07:33:43 +0100

    Finish day 3

--- aoc24/day3/solution.py
+++ aoc24/day3/solution.py
@@ -1,14 +1,50 @@
-from typing import List
+import re
+
+mul_regex = re.compile(r"^mul\((\d+),(\d+)\)")
 
 
-def part1(lines: List[str]) -> None:
-    pass
+def part1(input: list[str]) -> None:
+    sum = 0
+    for line in input:
+        while line:
+            match = mul_regex.match(line)
+            if match:
+                a, b = map(int, match.groups())
+                sum += a * b
+                line = line[match.end() :]
+                continue
+
+            line = line[1:]
+
+    print("Part 1:", sum)
 
 
-def part2(lines: List[str]) -> None:
-    pass
+def part2(input: list[str]) -> None:
+    sum = 0
+    mul_enabled = True
+    for line in input:
+        while line:
+            if line.startswith("do()"):
+                mul_enabled = True
+                line = line[4:]
+                continue
+            if line.startswith("don't()"):
+                mul_enabled = False
+                line = line[7:]
+                continue
+
+            match = mul_regex.match(line)
+            if mul_enabled and match:
+                a, b = map(int, match.groups())
+                sum += a * b
+                line = line[match.end() :]
+                continue
+
+            line = line[1:]
+
+    print("Part 2:", sum)
 
 
-def main(lines: List[str]) -> None:
+def main(lines: list[str]) -> None:
     part1(lines)
     part2(lines)
