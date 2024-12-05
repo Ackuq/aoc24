880672e722ff9a5cd2d207253a08a284cb695e73
Axel Pettersson <axel.pettersson@northvolt.com>
2024-12-05 08:09:34 +0100

    Finish day 5

--- aoc24/day5/solution.py
+++ aoc24/day5/solution.py
@@ -1,14 +1,74 @@
-from typing import List
+# Ordering x -> y means that x should come before y in the sorted list
+from functools import cmp_to_key
+
+Orderings = dict[int, set[int]]
 
 
-def part1(lines: List[str]) -> None:
-    pass
+Sequence = list[int]
+Sequences = list[Sequence]
 
 
-def part2(lines: List[str]) -> None:
-    pass
+def parse_input(lines: list[str]) -> tuple[Orderings, Sequences]:
+    empty_line = lines.index("\n")
+
+    ordering_lines = lines[:empty_line]
+    sequence_lines = lines[empty_line + 1 :]
+
+    orderings = {}
+    for line in ordering_lines:
+        x, y = map(int, line.strip().split("|"))
+        if x not in orderings:
+            orderings[x] = set()
+        orderings[x].add(y)
+
+    sequences = [list(map(int, line.strip().split(","))) for line in sequence_lines]
+
+    return orderings, sequences
 
 
-def main(lines: List[str]) -> None:
+def compare(orderings: Orderings):
+    def cmp(x: int, y: int) -> int:
+        if x in orderings and y in orderings[x]:
+            return -1
+        if y in orderings and x in orderings[y]:
+            return 1
+        return 0
+
+    return cmp
+
+
+def part1(lines: list[str]) -> None:
+    orderings, sequences = parse_input(lines)
+
+    correctly_sorted: Sequences = [
+        sequence
+        for sequence in sequences
+        if sorted(sequence, key=cmp_to_key(compare(orderings))) == sequence
+    ]
+
+    result = sum(sequence[len(sequence) // 2] for sequence in correctly_sorted)
+
+    print("Part 1:", result)
+
+
+def part2(lines: list[str]) -> None:
+    orderings, sequences = parse_input(lines)
+
+    sorted_lists = [
+        sorted(sequence, key=cmp_to_key(compare(orderings))) for sequence in sequences
+    ]
+
+    incorrect_sorted = [
+        correct
+        for correct, original in zip(sorted_lists, sequences)
+        if correct != original
+    ]
+
+    result = sum(sequence[len(sequence) // 2] for sequence in incorrect_sorted)
+
+    print("Part 2:", result)
+
+
+def main(lines: list[str]) -> None:
     part1(lines)
     part2(lines)
