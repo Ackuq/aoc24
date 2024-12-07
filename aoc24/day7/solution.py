92424e56fab3491a13aa58d5dd4f681894d06708
Axel Pettersson <axel.pettersson@northvolt.com>
2024-12-07 12:56:38 +0100

    Finish day 7

--- aoc24/day7/solution.py
+++ aoc24/day7/solution.py
@@ -1,14 +1,55 @@
-from typing import List
+from operator import add, mul
+from typing import Callable
+
+Equations = list[tuple[int, list[int]]]
+Operator = Callable[[int, int], int]
 
 
-def part1(lines: List[str]) -> None:
-    pass
+def parse(lines: list[str]) -> Equations:
+    splitted = [line.strip().split(": ") for line in lines]
+    equations: Equations = [
+        (int(line[0]), [int(num) for num in line[1].split(" ")]) for line in splitted
+    ]
+
+    return equations
 
 
-def part2(lines: List[str]) -> None:
-    pass
+def concat(a: int, b: int) -> int:
+    return int(f"{a}{b}")
 
 
-def main(lines: List[str]) -> None:
-    part1(lines)
-    part2(lines)
+def is_solvable(
+    answer: int, digits: list[int], operators: list[Operator], acc: set[int] = {0}
+) -> bool:
+    if not digits:
+        return answer in acc
+
+    acc = {op(prev, digits[0]) for prev in acc for op in operators}
+
+    return is_solvable(answer, digits[1:], operators, acc)
+
+
+def part1(equations: Equations) -> None:
+    solvable = sum(
+        answer
+        for answer, digits in equations
+        if is_solvable(answer, digits, [add, mul])
+    )
+
+    print("Part 1:", solvable)
+
+
+def part2(equations: Equations) -> None:
+    solvable = sum(
+        answer
+        for answer, digits in equations
+        if is_solvable(answer, digits, [add, mul, concat])
+    )
+
+    print("Part 2:", solvable)
+
+
+def main(lines: list[str]) -> None:
+    equations = parse(lines)
+    part1(equations)
+    part2(equations)
