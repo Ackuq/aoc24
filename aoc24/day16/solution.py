f56ef0c433cbcbd1d78b780b95967bf0994588a0
Axel Pettersson <axel.pettersson@northvolt.com>
2024-12-16 17:19:38 +0100

    Finish day 16 part 2

--- aoc24/day16/solution.py
+++ aoc24/day16/solution.py
@@ -63,7 +63,9 @@ class PrioritizedItem:
     direction: Direction = field(compare=False)
 
 
-def djikstra(lines: list[str], start: Coord, end: Coord) -> int:
+def djikstra(
+    lines: list[str], start: Coord, end: Coord
+) -> tuple[int, dict[Coord, int]]:
     distances: dict[Coord, int] = {start: 0}
 
     queue: PriorityQueue[PrioritizedItem] = PriorityQueue()
@@ -81,18 +83,90 @@ def djikstra(lines: list[str], start: Coord, end: Coord) -> int:
                 queue.put(PrioritizedItem(cost, new_pos, new_direction))
 
         if node.coord == end:
-            return node.distance
+            return node.distance, distances
 
 
 def part1(lines: list[str]) -> None:
     start, end = get_start_and_end(lines)
-    min_steps = djikstra(lines, start, end)
+    min_steps, _ = djikstra(lines, start, end)
 
     print("Part 1:", min_steps)
 
 
+def get_neighbors_nodes(
+    nodes: set[Coord], coord: Coord, dir: Direction
+) -> list[tuple[Coord, Direction, int]]:
+    neighbors: list[tuple[Coord, Direction, int]] = []
+    x, y = coord
+
+    for direction in direction_list:
+        dx, dy = direction.value
+        new_x, new_y = x + dx, y + dy
+        if (new_x, new_y) not in nodes:
+            continue
+
+        cost = abs(direction_list.index(dir) - direction_list.index(direction)) % 4
+        if cost == 3:
+            cost = 1
+
+        neighbors.append(((new_x, new_y), direction, cost * 1000))
+
+    return neighbors
+
+
+def dfs(
+    current: CoordWithDirection,
+    end: Coord,
+    nodes: set[Coord],
+    visited: set[Coord],
+    current_steps: int,
+    distances: dict[Coord, int],
+    min_steps: int,
+) -> set[Coord] | None:
+    coord, direction = current
+
+    # Makes sure we do not make a unnecessary turn
+    if current_steps > distances[coord] + 1000:
+        return None
+
+    visited = visited | {coord}
+
+    if coord == end:
+        return visited
+
+    if current_steps >= min_steps:
+        return None
+
+    neighbors = get_neighbors_nodes(nodes, coord, direction)
+    result = set[Coord]()
+
+    for new_pos, new_dir, turn_cost in neighbors:
+        if new_pos in visited:
+            continue
+
+        solution = dfs(
+            (new_pos, new_dir),
+            end,
+            nodes,
+            visited,
+            current_steps + turn_cost + 1,
+            distances,
+            min_steps,
+        )
+
+        if solution is not None:
+            result = result | solution
+
+    return result
+
+
 def part2(lines: list[str]) -> None:
-    pass
+    start, end = get_start_and_end(lines)
+    min_steps, distances = djikstra(lines, start, end)
+    nodes = {coord for coord, distance in distances.items() if distance <= min_steps}
+    visited = dfs((start, Direction.East), end, nodes, set(), 0, distances, min_steps)
+    assert visited is not None
+    print("Part 2:", len(visited))
 
 
 def main(lines: list[str]) -> None:
