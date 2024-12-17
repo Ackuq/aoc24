df7cfdba7559e620e3434de4e5c0313dd0b51285
Axel Pettersson <axel.pettersson@northvolt.com>
2024-12-17 09:13:06 +0100

    Finish day 17 part 2

--- aoc24/day17/solution.py
+++ aoc24/day17/solution.py
@@ -26,43 +26,44 @@ def parse_input(lines: list[str]) -> Input:
 
 
 def combo_operand(operand: int, register: Registers) -> int:
-    if operand >= 0 and operand <= 3:
-        return operand
-    if operand == 4:
-        return register["A"]
-    if operand == 5:
-        return register["B"]
-    if operand == 6:
-        return register["C"]
-
+    match operand:
+        case 0 | 1 | 2 | 3:
+            return operand
+        case 4:
+            return register["A"]
+        case 5:
+            return register["B"]
+        case 6:
+            return register["C"]
     assert False
 
 
 def run_instruction(
     instruction: int, register: Registers, operand: int
 ) -> tuple[int | None, bool]:
-    if instruction == 0:
-        combo = combo_operand(operand, register)
-        register["A"] = register["A"] // (2**combo)
-    if instruction == 1:
-        register["B"] = register["B"] ^ operand
-    if instruction == 2:
-        combo = combo_operand(operand, register)
-        register["B"] = combo % 8
-    if instruction == 3:
-        if register["A"] == 0:
-            return None, False
-        return None, True
-    if instruction == 4:
-        register["B"] = register["B"] ^ register["C"]
-    if instruction == 5:
-        return combo_operand(operand, register) % 8, False
-    if instruction == 6:
-        combo = combo_operand(operand, register)
-        register["B"] = register["A"] // (2**combo)
-    if instruction == 7:
-        combo = combo_operand(operand, register)
-        register["C"] = register["A"] // (2**combo)
+    match instruction:
+        case 0:
+            combo = combo_operand(operand, register)
+            register["A"] = register["A"] // (2**combo)
+        case 1:
+            register["B"] = register["B"] ^ operand
+        case 2:
+            combo = combo_operand(operand, register)
+            register["B"] = combo % 8
+        case 3:
+            if register["A"] == 0:
+                return None, False
+            return None, True
+        case 4:
+            register["B"] = register["B"] ^ register["C"]
+        case 5:
+            return combo_operand(operand, register) % 8, False
+        case 6:
+            combo = combo_operand(operand, register)
+            register["B"] = register["A"] // (2**combo)
+        case 7:
+            combo = combo_operand(operand, register)
+            register["C"] = register["A"] // (2**combo)
 
     return None, False
 
@@ -83,23 +84,30 @@ def part1(input: Input, print_result: bool = True) -> str:
             i = operand
         else:
             i += 2
-    result = ",".join(str(num) for num in output)
+    result = ",".join(map(str, output))
     if print_result:
         print("Part 1:", result)
     return result
 
 
+def find_lowest_solution(instructions: list[int], target: str) -> int:
+    to_check = [(len(instructions) - 1, 0)]
+    for pos, val in to_check:
+        start = val * 8
+        end = val * 8 + 8
+        target_sub = ",".join(map(str, instructions[pos:]))
+        for a in range(start, end):
+            res = part1(({"A": a, "B": 0, "C": 0}, instructions), print_result=False)
+            if res == target_sub:
+                to_check.append((pos - 1, a))
+                if pos == 0:
+                    return a
+    assert False
+
+
 def part2(input: Input, target: str) -> None:
-    register, instructions = input
-    solution = 0
-    while True:
-        new_register = deepcopy(register)
-        new_register["A"] = solution
-        result = part1((new_register, instructions), False)
-        if result == target:
-            print("Part 2:", solution)
-            return
-        solution += 1
+    min_solution = find_lowest_solution(input[1], target)
+    print("Part 2:", min_solution)
 
 
 def main(lines: list[str]) -> None:
